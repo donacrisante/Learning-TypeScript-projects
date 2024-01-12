e458a9fb4f3988f7313bb3ea7b02986cb62ed7cd
dona <>
2024-01-12 15:09:08 +0100

    make challenge callbacks to async-await

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,39 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+async function checkEmotion(knownEmotions, emotion) {
+	await new Promise((resolve) => {
+		return setTimeout(resolve, 1000);
+	});
+
+	return knownEmotions.has(emotion);
+
+	/* return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(knownEmotions.has(emotion));
+        }, 1000);
+    })	 */
+}
+
+async function speak(knownEmotions, newEmotion, phrase) {
+	if (!(await checkEmotion(knownEmotions, newEmotion))) {
+		throw new Error(`Does not compute. I do not understand ${newEmotion}.`);
+	}
+
+	return `"${phrase}" (${newEmotion})`;
+
+	/* try {
+        const hasEmotion = await checkEmotion(knownEmotions, newEmotion);
+
+		if (hasEmotion) {
+		  return `"${phrase}" (${newEmotion})`;
+		} else {
+		  throw new Error(`Does not compute. I do not understand ${newEmotion}.`)
+		}
+    } catch (error) {
+        throw error;
+    } */
+}
+
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
